9bff7955bf6a1356b9c3eb72ad497d664b706a88
hamza ezzat <hamza2007ezzat@gmail.com>
2023-02-19 10:29:07 +0100

    modify login to store data in DB

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,7 +1,9 @@
 import User from '../mongodb/models/user.js'
 
 const getAllUsers = async(req,res)=>{};
-const createUser = async(req,res)=>{};
+const createUser = async(req,res)=>{
+    
+};
 const getUserInfoByID = async(req,res)=>{};
 
 export {
