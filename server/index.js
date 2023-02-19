eec7ced092abfa8d5d30f10a97a5e8575ab977b8
hamza ezzat <hamza2007ezzat@gmail.com>
2023-02-19 08:01:21 +0100

    connected to MongoDB and mongoos

---
+++ server/index.js
@@ -0,0 +1,27 @@
+// Routes
+import express from 'express';
+// to use enviroment variable
+import * as dotenv from 'dotenv'
+import cors from 'cors';
+
+import connectDB from './mongodb/connect.js';
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json({limit:'50mb'}));
+
+app.get('/',(req,res)=>{
+    res.send({message:'Hello World!'})
+})
+
+const startServer = async ()=>{
+    try{
+        // Connect to Database...
+        connectDB(process.env.MONGODB_URL)
+        app.listen(8080,()=>console.log('Server started on port http://localhost:8080'))
+    }catch (error){
+        console.log(error)
+    }   
+}
+startServer();
