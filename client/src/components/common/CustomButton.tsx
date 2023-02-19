2f803aef0b7eccf38949f515dfc1a813dc7c39bb
hamza ezzat <hamza2007ezzat@gmail.com>
2023-02-19 17:42:19 +0100

    Sort, filter, show items

--- client/src/components/common/CustomButton.tsx
+++ client/src/components/common/CustomButton.tsx
@@ -3,20 +3,21 @@ import { Button } from '@pankod/refine-mui'
 import { CustomButtonProps } from 'interfaces/common'
 import React from 'react'
 
-const CustomButton = ({type,title,backgroundColor,color,fullWidth,icon,handleClick}:CustomButtonProps) => {
+const CustomButton = ({type,title,backgroundColor,color,fullWidth,icon, handleClick, disabled}:CustomButtonProps) => {
   return (
     <Button
+    disabled = {disabled}
     type={type === 'submit' ? 'submit': 'button'}
     sx={{
       flex:fullWidth ? 1: 'unset',
       padding:'10px 15px',
       width:fullWidth ? '100%' : "fit-content",
+      gap: '10px',
+      fontWeight: 600,
       minWidth: 130,
+      fontSize: 16,
       backgroundColor,
       color,
-      fontSize: 16,
-      fontWeight: 600,
-      gap: '10px',
       textTransform: "capitalize",
       '&:hover':{
         opacity: '0.9',
